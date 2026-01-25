7cb54461b1399ac48943c40a89209d78a929a99e
Khushi <k23verma@gmail.com>
2026-01-25 19:08:16 +0530

    Developed header and footer

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -7,6 +7,13 @@ export default function MainPage() {
     return(
         <>
         <Header />
+        <div className="intro">
+            <h1>HEY, I’M KHUSHI VERMA</h1>
+            <img src="/port-image.png" alt="Image of Khushi Verma"/>
+        </div>
+        <div className="one-liner"></div>
+        <div className="carousel"></div>
+        <div className="timeline"></div>
         </>
     );
 }
