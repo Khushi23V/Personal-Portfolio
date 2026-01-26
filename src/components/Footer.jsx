a614c933e52c36c1c310f28bf2c2e420f4a129ea
Khushi <k23verma@gmail.com>
2026-01-26 16:08:19 +0530

    Further development of Home Page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom";
 
 export default function Footer(){
     return (
-        <footer>
-            
+        <footer className="footer">
+            <p>Designed and Developed by Khushi Verma</p>
         </footer>
     );
 }
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,19 +1,45 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import Header from "./Header";
-
+import Footer from "./Footer";
 
 export default function MainPage() {
     return(
         <>
         <Header />
+
         <div className="intro">
-            <h1>HEY, I’M KHUSHI VERMA</h1>
-            <img src="/port-image.png" alt="Image of Khushi Verma"/>
+            <h1 className="hero-title">
+                <span className="line hey">HEY<span className="accent">,</span></span>
+                <span className="line name">I<span className="accent">’</span>M KHUSHI</span>
+                <span className="line surname">VERMA</span>
+            </h1>
+<div className="image-logo">
+  <img className="profile-img" src="/port-image.png" alt="Image of Khushi Verma"
+  />
+  <div className="icons">
+    <a href="https://www.linkedin.com/in/khushiverma23/" target="_blank" rel="noreferrer">
+      <img src="/linkedin-logo.png" alt="LinkedIn" />
+    </a>
+    <a href="https://mail.google.com/mail/?view=cm&fs=1&to=k23verma@gmail.com" className="gmail" target="_blank" rel="noreferrer">
+      <img src="/gmail.png" alt="Gmail"  />
+    </a>
+    <a href="https://github.com/Khushi23V" target="_blank" rel="noreferrer">
+      <img src="/github.png" alt="GitHub" />
+    </a>
+  </div>
+</div>
         </div>
-        <div className="one-liner"></div>
+
+        <div className="one-liner">
+            <h2 className="opening">&lt;</h2>
+            <h2 className="text">I’m a Computer Science student who’s slightly obsessed with making the web look and feel amazing.
+                <span className="text-break"> Click here to download my resume!</span></h2>
+            <h2 className="closing">/&gt;</h2>
+        </div>
         <div className="carousel"></div>
         <div className="timeline"></div>
+        <Footer />
         </>
     );
 }
