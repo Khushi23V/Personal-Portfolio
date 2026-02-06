bb523807c1489c05692c1f59b84aa18b77c313c4
Khushi <k23verma@gmail.com>
2026-02-06 20:54:28 +0530

    Further development of Home Page

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -37,8 +37,57 @@ export default function MainPage() {
                 <span className="text-break"> Click here to download my resume!</span></h2>
             <h2 className="closing">/&gt;</h2>
         </div>
-        <div className="carousel"></div>
-        <div className="timeline"></div>
+        <div className="quick-links">
+          <h2>Quick Links<span className="accent">.</span></h2>
+          <p>Check out my two different personalities:</p>
+          <div className="link-btns">
+            <a href="https://github.com/Khushi23V" target="_blank" rel="noreferrer"><button className="btn-1">Developer</button></a>
+            <a href="https://www.figma.com/@khushiv" target="_blank" rel="noreferrer"><button className="btn-2">Designer</button></a>
+          </div>
+        </div>
+        <div className="work">
+          <h2>Stuff I've Built<span className="accent">.</span></h2>
+          <div className="work-1">
+            <img className="p1-img" src="/p-1.png" alt="Image of summarAIze"
+  />
+            <div className="project-info">
+              <div className="p-heading">
+                <h2>summarAIze</h2>
+                <div className="p-links">
+                  <a href="https://github.com/Khushi23V" target="_blank" rel="noreferrer">
+      <img src="/github.png" alt="GitHub" />
+    </a>
+    <a href="https://summaraize-ai.in/" target="_blank" rel="noreferrer">
+      <img src="/arrow.png" alt="Link" className="arrow"/>
+    </a>
+                </div>
+              </div>
+              <p className="p-desc">In aliquet turpis varius nibh ullamcorper imperdiet. Curabitur mauris eros, finibus nec mattis id, eleifend vitae est. Curabitur vel risus sapien.
+</p>
+              <div className="p-tech">
+                <button>React.js</button>
+                <button>Vite</button>
+                <button>CSS</button>
+                <button>Node.js</button>
+                <button>Express.js</button>
+                <button>CORS</button>
+                <button>dotenv</button>
+                <button>REST API</button>
+                <button>Hugging Face API</button>
+                <button>Axios</button>
+                <button>Clarity</button>
+              </div>
+
+            </div>
+          </div>
+          <div className="view-all">
+            <Link to="/projects" className="nav-link">
+            <p>View all</p>
+            <img src="/arrow-2.png" alt="Link" className="arrow"/>
+            </Link>
+          </div>
+        </div>
+        <div className="timeline curve-bottom"></div>
         <Footer />
         </>
     );
